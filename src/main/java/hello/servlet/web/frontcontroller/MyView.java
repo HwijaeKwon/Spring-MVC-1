8bc3a42f4515aff0c349e63af159922761493e50
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-04-22 23:52:33 +0900

    [feat] ControllerV3 구현
    - ModelView 추가
    - Controller에 Servlet과 관련된 로직을 전부 제거

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -14,8 +15,21 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
+    /**
+     * 화면을 렌더링한다 (직접 렌더링할 수도 있고, JSP를 이용할 수도 있다)
+     */
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach(request::setAttribute);
     }
 }
